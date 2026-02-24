4797623af0f3102a535a2ed79abe45474fb4cf64
Omar Elsayed <eng.omarelsayed@deveagency.com>
2026-02-24 16:37:16 +0200

    Update layout.tsx

--- Website/cleanbrowse-website/src/app/layout.tsx
+++ Website/cleanbrowse-website/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -41,6 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.variable} antialiased`}>{children}</body>
+      <Analytics/>
     </html>
   );
 }
